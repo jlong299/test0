6694fe13d629c1a0ee6001460168446e92d210db
jlong299 <jlong299@foxmail.com>
2016-07-22 12:01:03 +0800

    add descrip . add test.v

---
+++ test.v
@@ -0,0 +1,19 @@
+module test
+	(
+		input 		clk,
+		input 		rst_n,
+
+		output reg [3:0] 	cnt
+		);
+
+always@(posedge clk)
+begin
+	if (!rst_n)
+		cnt <= 0;
+	else
+	begin
+		cnt <= (cnt == 4'd15) ? 4'd0 : cnt + 4'd1;
+	end
+end
+
+endmodule
